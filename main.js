aa32d0ae3a19f14f5121a4eb3e988619a5037318
yosser16 <yossermerida@outlook.fr>
2024-05-14 11:14:21 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// Vérificateur d'année bissextile  : 
+// function bissextile(year) {
+//     if (year %4 == 0 && year %100 !== 0 || year % 400 === 0) { 
+      
+//     console.log("year is bissextile"); }
+
+//     else 
+//     console.log("year is not  bissextile");
+// } 
+
+// bissextile(1919) 
+
+//Tarification des billets : 
+
+// function tickets(age) {
+//     if ( age<= 12) {
+//         console.log("ticket is 10$");
+//     }
+//     else if (age >= 13 && age <=17  ) 
+//          {    console.log("ticket is 15$");} 
+//     else {
+//         console.log("ticket is 20$");
+//     }
+// }
+//  tickets(24)
+
+// Palindrome Checker : 
+// function Palindrome(str) { 
+//     return str.split('').reverse('').join('')===str ;
+    
+// }
+//  console.log(Palindrome("youyou")) 
+
+
+// function recursing  (n){
+//         if ( n < 2)
+//             { return n }
+//         return recursing(n-1)+ recursing(n-2)
+        
+//     }
+    
+//     console.log(recursing(1));
+    
